51cf175cb9b7b79447d090a3908e23188447e503
Ubuntu <ubuntu@ip-172-31-13-137.ap-south-1.compute.internal>
2023-08-18 04:27:09 +0000

    added 6 but not working

---
+++ list_1/6/copy_file.cpp
@@ -0,0 +1,14 @@
+#include<unistd.h>
+#include<iostream>
+#include<fcntl.h>
+using namespace std;
+int main(){
+    int reader, writer, i;
+    char buf[1024];
+    reader  = open("somestuff",O_RDONLY);
+    writer = open("copiedstuff",O_CREAT);
+    do{
+        i = read(reader,buf,sizeof(buf));
+        if(i) write(writer, buf, i);
+}while(i);
+}
