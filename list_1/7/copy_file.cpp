e460942ff9541df14c5b794397550c25bb4ad967
Ubuntu <ubuntu@ip-172-31-13-137.ap-south-1.compute.internal>
2023-08-20 09:26:42 +0000

    some updates

--- list_1/7/copy_file.cpp
+++ list_1/7/copy_file.cpp
@@ -2,11 +2,15 @@
 #include<iostream>
 #include<fcntl.h>
 using namespace std;
-int main(){
+int main(int argc, char *argv[]){
     int reader, writer, i;
     char buf[1024];
-    reader  = open("somestuff",O_RDONLY);
-    writer = open("copiedstuff",O_CREAT|O_WRONLY,666);
+    if(argc != 3) {
+        cout<<"Wrong No of arguments provided"<<endl;return 0;
+    }
+    char *read_file = argv[1], *write_file = argv[2];
+    reader  = open(read_file,O_RDONLY);
+    writer = open(write_file,O_CREAT|O_WRONLY,00666);
     do{
         i = read(reader,&buf,sizeof(buf));
         if(i) write(writer, &buf, i);
