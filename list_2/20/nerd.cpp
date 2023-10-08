2102551946d86970a92545a7325784dec93f788f
gururajo <gururajotagerimail2@gmail.com>
2023-10-09 01:00:16 +0530

    some dates

--- list_2/20/nerd.cpp
+++ list_2/20/nerd.cpp
@@ -9,7 +9,7 @@ int main(){
     char buf[1024];
     fd = open("messenger", O_RDWR);
     fd2 = open("messenger2", O_RDWR);
-    cout<<"Typing..:Do you want to have gooooood s*x?\n\n";
+    cout<<"Typing..:Do you want to have gooooood time?\n\n";
     write(fd, "N:Do you want to have gooooood s*x?\n\n", sizeof( "N:Do you want to have gooooood s*x?\n\n"));
     sleep(3);
     read(fd2, buf, sizeof(buf));
